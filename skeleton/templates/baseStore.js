0d7f6a1f3a1d7b06e0efc92d32cc4b45ff761742
Wyatt Pultz <wyatt@mobelux.com>
2017-01-09 15:35:01 -0500

    a few tweaks after actually using this a little

--- skeleton/templates/baseStore.js
+++ skeleton/templates/baseStore.js
@@ -1,20 +1,28 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import rootReducer from '../reducers';
 
 const createStoreWithMiddleware = applyMiddleware(
-    thunkMiddleware
+  thunkMiddleware
 )(createStore);
 
 export default function configureStore(initialState) {
-    const store = createStoreWithMiddleware(rootReducer, initialState);
+  // const store = createStoreWithMiddleware(rootReducer, initialState);
+  const store = createStore(
+    rootReducer,
+    initialState,
+    compose(
+      applyMiddleware(thunkMiddleware),
+      typeof window === 'object' && typeof window.devToolsExtension !== 'undefined' ? window.devToolsExtension() : f => f
+    )
+  )
 
-    if (module.hot) {
-        module.hot.accept('../reducers', () => {
-            const nextRootReducer = require('../reducers');
-            store.replaceReducer(nextRootReducer);
-        });
-    }
+  if (module.hot) {
+    module.hot.accept('../reducers', () => {
+      const nextRootReducer = require('../reducers');
+      store.replaceReducer(nextRootReducer);
+    });
+  }
 
-    return store;
-};
+  return store;
+}
